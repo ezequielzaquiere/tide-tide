705bfa0987ac63f6ac6dcba1d89534027eda3c5c
Ezequiel Zaquiere <ezequielzaquiere@gmail.com>
2025-03-25 16:51:45 +0100

    feat: Button Component + Colors CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import Header from './pages/Header';
 import MarineMap from './components/MarineMap';
 import Footer from './pages/Footer';
+import Button from './components/Button';
 
 export default function App() {
     return (
         <div className='h-screen flex flex-col'>
             <Header />
+            <Button variant='primary'>Ver pronóstico</Button>
+            <Button variant='secondary'>Opciones</Button>
+            <Button variant='alert'>Cancelar</Button>
             <MarineMap />
             <Footer />
         </div>
